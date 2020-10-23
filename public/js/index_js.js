b836ed43770664ae305ec99add07dada0ae1ba40
Gagan Chaudhary <46825685+Gagan-Chaudhary@users.noreply.github.com>
2020-10-23 18:23:00 +0530

    Add files via upload

---
+++ public/js/index_js.js
@@ -0,0 +1,22 @@
+function openWin() {
+    window.open("/login","_self");
+  }
+
+function openWin2() {
+    window.open("/signup","_self");
+  }  
+
+  		// $(document).ready(function(){
+			// 	$('div#loading').removeAttr('id');
+		// });
+		var preloader = document.getElementById("loading");
+		// window.addEventListener('load', function(){
+		// 	preloader.style.display = 'none';
+		// 	})
+
+		function myFunction(){
+			preloader.style.display = 'none';
+    };
+    
+
+    
